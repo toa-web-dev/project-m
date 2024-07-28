d304af88c916e01e3e90274115c3d7f30d00d5b5
gimHyoungSoo <gimhyoungsoo@gmail.com>
2024-07-28 17:50:58 +0900

    feat Recordplate - 레코드 요소 생성 - 스타일 및 회전 애니메이션

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,10 @@
+import Recordplate from "../src/components/Recordplate";
 function App() {
-
-  return (
-    <>
-      
-    </>
-  )
+    return (
+        <>
+            <Recordplate></Recordplate>
+        </>
+    );
 }
 
-export default App
+export default App;
--- src/components/Recordplate.jsx
+++ src/components/Recordplate.jsx
@@ -1,10 +1,53 @@
+import styled, { keyframes } from "styled-components";
+
 function Recordplate() {
-  
-  return <>
-  <div className="plate">
-    <div className="label">
-      <img src="" alt="레코드 레이블" />
-    </div>
-  </div>
-  </>
+    const rotate = keyframes`
+    from {
+      transform: rotate(0deg);
+    }
+    to {
+      transform: rotate(360deg);
+    }
+  `;
+
+    const Plate = styled.div`
+        animation: ${rotate} 5s linear infinite;
+        width: 200px;
+        height: 200px;
+        background-color: black;
+        border-radius: 100%;
+        display: flex;
+        justify-content: center;
+        align-items: center;
+    `;
+    const Label = styled.div`
+        width: 120px;
+        height: 120px;
+        background-image: url(../../public/14947269.jpg);
+        background-position: center;
+        background-size: 100%;
+        background-repeat: no-repeat;
+        background-color: white;
+        border-radius: 100%;
+        display: flex;
+        justify-content: center;
+        align-items: center;
+    `;
+    const Centerhole = styled.div`
+        width: 10px;
+        height: 10px;
+        border-radius: 100%;
+        background-color: white;
+    `;
+
+    return (
+        <>
+            <Plate>
+                <Label>
+                    <Centerhole></Centerhole>
+                </Label>
+            </Plate>
+        </>
+    );
 }
+export default Recordplate;
